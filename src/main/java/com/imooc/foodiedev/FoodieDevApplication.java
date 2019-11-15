d64ea958b5d088378fa7acbb4292c42f270e39b6
yan.lu <934296024@163.com>
2019-11-15 13:47:48 +0800

    Initial commit

---
+++ src/main/java/com/imooc/foodiedev/FoodieDevApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.foodiedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/foodiedev/FoodieDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.foodiedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
