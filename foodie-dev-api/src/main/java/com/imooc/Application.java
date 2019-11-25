193803a56fd04e7a6fb6215fcc391a9d789c201d
yan.lu <934296024@163.com>
2019-11-25 09:27:03 +0800

    spring事务传播机制

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -5,12 +5,14 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author 陆炎
  * @version : Application.java, v0.1 2019/11/16 陆炎 Exp $$
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
 
     public static void main(String[] args) {
