47265e933d4d84c9b72432fb10fdad4cfffee4d4
yan.lu <934296024@163.com>
2019-11-27 15:41:42 +0800

    用户注册

--- foodie-dev-service/src/main/java/com/imooc/service/UserService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -1,5 +1,8 @@
 package com.imooc.service;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+
 /**
  * @author 陆炎
  * @version : UserService.java, v0.1 2019/11/27 陆炎 Exp $$
@@ -8,6 +11,15 @@ public interface UserService {
 
     /**
      * 判断用户名是否存在
+     * @param userName
+     * @return boolean
      */
     boolean queryUserNameIsExist(String userName);
+
+    /**
+     * 新建用户
+     * @param userBO
+     * @return Users
+     */
+    Users createUser(UserBO userBO);
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -3,12 +3,21 @@
  */
 package com.imooc.service.impl;
 
+import com.imooc.enums.Sex;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
+import com.imooc.utils.DateUtil;
+import com.imooc.utils.MD5Utils;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
 
 /**
  * @author 陆炎
@@ -17,8 +26,12 @@ import tk.mybatis.mapper.entity.Example;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final String USER_FACE = "http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_8_qAIlFXAAAcIhVPdSg994.png";
+
     @Autowired
     private UsersMapper usersMapper;
+    @Autowired
+    private Sid sid;
 
     @Override
     public boolean queryUserNameIsExist(String userName) {
@@ -28,5 +41,42 @@ public class UserServiceImpl implements UserService {
             }
         });
         return users == null ? false : true;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users createUser(UserBO userBO) {
+
+//        try {
+//            Thread.sleep(3500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        String userId = sid.nextShort();
+
+        Users user = new Users();
+        user.setId(userId);
+        user.setUsername(userBO.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 默认用户昵称同用户名
+        user.setNickname(userBO.getUsername());
+        // 默认头像
+        user.setFace(USER_FACE);
+        // 默认生日
+        user.setBirthday(DateUtil.stringToDate("1900-01-01"));
+        // 默认性别为 保密
+        user.setSex(Sex.secret.type);
+
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
+
+        usersMapper.insert(user);
+
+        return user;
     }
 }
