fe88e589b014cea6522a716151bef5c66a65ac33
yan.lu <934296024@163.com>
2019-11-28 17:14:13 +0800

    '查询分类'

---
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2019-2019 All Rights Reserved.
+ */
+package com.imooc.service.impl;
+
+import com.imooc.mapper.CategoryMapper;
+import com.imooc.mapper.CategoryMapperCustom;
+import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+/**
+ * @author 陆炎
+ * @version : CategoryServiceImpl.java, v0.1 2019/11/28 陆炎 Exp $$
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Autowired
+    private CategoryMapperCustom categoryMapperCustom;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<Category> queryAllRootLevelCat() {
+
+        Example example = new Example(Category.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("type", 1);
+
+        List<Category> result =  categoryMapper.selectByExample(example);
+
+        return result;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<CategoryVO> getSubCatList(Integer rootCatId) {
+        return categoryMapperCustom.getSubCatList(rootCatId);
+    }
+}
